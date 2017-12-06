da44f3b29d3dfcf30a94c2c3a5e827d09b85377b
Roman Michak <roman.michak@forbytes.com>
2017-12-06 14:35:09 +0200

    task 2 comments added

--- Model/Page.php
+++ Model/Page.php
@@ -12,7 +12,12 @@ class Training_Cms_Model_Page extends Mage_Core_Model_Abstract
         $this->_init('training_cms/page');
     }
 
-
+    /**
+     * Loading page by code
+     *
+     * @param $code
+     * @return Mage_Core_Model_Abstract
+     */
     public function loadByCode($code)
     {
         return $this->load($code, 'code');
