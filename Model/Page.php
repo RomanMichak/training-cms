a596850009a7fe89437d3d03d5f820cac503bed3
Roman Michak <roman.michak@forbytes.com>
2017-12-06 14:34:46 +0200

    task 2

--- Model/Page.php
+++ Model/Page.php
@@ -1,5 +1,22 @@
 <?php
 
-class Training_Cms_Model_Page extends Mage_Core_Model_Abstract{
+class Training_Cms_Model_Page extends Mage_Core_Model_Abstract
+{
+
+    /**
+     * init model
+     *
+     */
+    protected function _construct()
+    {
+        $this->_init('training_cms/page');
+    }
+
+
+    public function loadByCode($code)
+    {
+        return $this->load($code, 'code');
+    }
+
 
 }
