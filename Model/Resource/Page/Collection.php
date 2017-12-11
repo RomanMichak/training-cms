88c50d6f28a86f299e66767f3ec74d2d98f542ed
Roman Michak <roman.michak@forbytes.com>
2017-12-11 12:00:07 +0200

    task 3 code style fixes

--- Model/Resource/Page/Collection.php
+++ Model/Resource/Page/Collection.php
@@ -2,11 +2,9 @@
 
 class Training_Cms_Model_Resource_Page_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
-
     public function __construct()
     {
         $this->_init('training_cms/page');
         parent::__construct();
     }
-
 }
